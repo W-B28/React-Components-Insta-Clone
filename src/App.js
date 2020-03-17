466ef26e267d619c46e87f9c350fbfb0b6d4c4df
William Bell <wtb2804@gmail.com>
2020-03-17 18:48:51 -0400

    Post and PostPage Complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-/* 
-Start here and then work through the 
-PostsContainer components and the CommentSection Components. 
-Not all files need code added. 
-Look at each file to see where you need to pass props or add code 
+/*
+Start here and then work through the
+PostsContainer components and the CommentSection Components.
+Not all files need code added.
+Look at each file to see where you need to pass props or add code
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer"
+import DummyData from "./dummy-data";
+
 // import the PostsPage and SearchBar and add them to the App
 
 
 const App = () => {
-
+  const [data, setData] = useState(DummyData);
+  // console.log("here is some data", data[0].username)
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostsPage data = {DummyData} />
     </div>
   );
 };
